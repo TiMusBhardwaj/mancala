c08068c93c43e83cda7bce74a69e5ee1383213b1
TiMusBhardwaj <bhardwaj.sumit016@gmail.com>
2022-02-07 19:46:30 +0530

    Update App.java

--- src/main/java/com/game/mancala/App.java
+++ src/main/java/com/game/mancala/App.java
@@ -2,14 +2,18 @@ package com.game.mancala;
 
 import com.game.mancala.frame.Game;
 
+import lombok.extern.log4j.Log4j2;
+
 /**
  * Hello world!
  *
  */
+@Log4j2
 public class App 
 {
     public static void main( String[] args )
     {
+    	log.info("");
        Game game = new Game();
        game.start();
     }
